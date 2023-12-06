cf09dbea6a233c64bdaf937f1a855fa8baf5c56c
DESKTOP-O0MN2LR\Павел <protagonyst@mail.ru>
2023-12-06 18:41:36 +0400

    1) курсор становится рукой при наведении  на заголовок таблицы 2) в файл requirements добавлены требования,  также исправлено имя файла 3) main.py просто отформатирован blackformatter'ом

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ models.Base.metadata.create_all(bind=engine)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 def get_db_session():
     db = SessionLocal()
     try:
@@ -25,14 +26,19 @@ def get_db_session():
     finally:
         db.close()
 
+
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
     return templates.TemplateResponse("index0.html", {"request": request})
 
+
 @app.get("/get-staff/", response_class=HTMLResponse)
-def get_staff(request: Request, db: Session = Depends(get_db_session), 
-              sort_column: str | None = None, is_sort_order_asc: bool | None = None
-              ):
+async def get_staff(
+    request: Request,
+    db: Session = Depends(get_db_session),
+    sort_column: str | None = None,
+    is_sort_order_asc: bool | None = None,
+):
     staff = None
     if sort_column == None:
         staff = db.query(models.Staff_DB).all()
@@ -45,26 +51,32 @@ def get_staff(request: Request, db: Session = Depends(get_db_session),
             staff = db.query(models.Staff_DB).order_by(needed_sort_column).all()
         else:
             staff = db.query(models.Staff_DB).order_by(desc(needed_sort_column)).all()
-    
-    return templates.TemplateResponse("table.html", {"request": request, "data": changeDateForShow(staff)})
+
+    return templates.TemplateResponse(
+        "table.html", {"request": request, "data": changeDateForShow(staff)}
+    )
+
 
 @app.post("/add")
 async def add_employee(employee: schema.Staff, db: Session = Depends(get_db_session)):
     added_employee = models.Staff_DB(
-                            first_name=employee.first_name,
-                            last_name=employee.last_name,
-                            address=employee.address, 
-                            birthdate=employee.birthdate)
+        first_name=employee.first_name,
+        last_name=employee.last_name,
+        address=employee.address,
+        birthdate=employee.birthdate,
+    )
     print(added_employee)
     db.add(added_employee)
     db.commit()
     db.refresh(added_employee)
 
+
 @app.post("/delete/{staff_id}")
-async def delete_employee(staff_id: int, db:Session = Depends(get_db_session)):
+async def delete_employee(staff_id: int, db: Session = Depends(get_db_session)):
     deleted_employee = db.query(models.Staff_DB).get(staff_id)
     db.delete(deleted_employee)
     db.commit()
+
 
 @app.get("/get/{staff_id}")
 async def get_person(staff_id: int, db: Session = Depends(get_db_session)):
@@ -72,9 +84,12 @@ async def get_person(staff_id: int, db: Session = Depends(get_db_session)):
     empl = jsonable_encoder(employee)
     return empl
 
+
 @app.post("/update/{staff_id}")
-async def update_employee(staff_id: int, employee: schema.Staff, db: Session = Depends(get_db_session)):
-    updated_employee: models.Staff_DB = db.query(models.Staff_DB).get(staff_id) # type: ignore
+async def update_employee(
+    staff_id: int, employee: schema.Staff, db: Session = Depends(get_db_session)
+):
+    updated_employee: models.Staff_DB = db.query(models.Staff_DB).get(staff_id)  # type: ignore
     updated_employee.first_name = employee.first_name
     updated_employee.last_name = employee.last_name
     updated_employee.address = employee.address
@@ -82,23 +97,34 @@ async def update_employee(staff_id: int, employee: schema.Staff, db: Session = D
     db.commit()
     db.refresh(updated_employee)
 
+
 @app.get("/search")
-def search_employee(request: Request, db: Session = Depends(get_db_session), 
-                    first_name: str | None = None, 
-                    last_name: str | None = None,
-                    address: str | None = None,
-                    ):
+async def search_employee(
+    request: Request,
+    db: Session = Depends(get_db_session),
+    first_name: str | None = None,
+    last_name: str | None = None,
+    address: str | None = None,
+):
     first_name = f"%{first_name}%"
     last_name = f"%{last_name}%"
     address = f"%{address}%"
-    employeeOrStaff = db.query(models.Staff_DB).\
-                                filter(models.Staff_DB.first_name.ilike(first_name),\
-                                       models.Staff_DB.last_name.ilike(last_name),\
-                                        models.Staff_DB.address.ilike(address)).\
-                                all()
-    
-    response = templates.TemplateResponse("tableSearch.html", {"request": request, "searched_staff": changeDateForShow(employeeOrStaff)})
+    employeeOrStaff = (
+        db.query(models.Staff_DB)
+        .filter(
+            models.Staff_DB.first_name.ilike(first_name),
+            models.Staff_DB.last_name.ilike(last_name),
+            models.Staff_DB.address.ilike(address),
+        )
+        .all()
+    )
+
+    response = templates.TemplateResponse(
+        "tableSearch.html",
+        {"request": request, "searched_staff": changeDateForShow(employeeOrStaff)},
+    )
     return response
+
 
 def changeDateForShow(staff):
     if staff:
